cdb07427fb5fb66f7c44af75c78ee4ba74cf54be
Abdallah Rasmy <100097511+abdourasmy@users.noreply.github.com>
2022-06-13 02:29:31 +0200

    Delete Jironis directory

--- Jironis/assets/js/custom.js
+++
@@ -1,6 +0,0 @@
-/*==================================================================================
-    Custom JS (Any custom js code you want to apply should be defined here).
-====================================================================================*/
-let data = document.getElementById("ss")
-let myData = new Date()
-data.innerHTML = myData.getFullYear()
